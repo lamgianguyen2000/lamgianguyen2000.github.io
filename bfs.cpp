a24235537ca3e99fe9b8bd21401e6d2e67d129c5
lamgianguyen2000 <lamgianguyen2008@gmail.com>
2025-11-24 14:08:37 +0700

    Implement breadth-first search algorithm in bfs.cpp

---
+++ bfs.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define N int(10e6)
+using namespace std;
+int n,m,s;
+int check[N];
+vector<int>g[N];
+queue<int>qu;
+void inp()
+{
+    cin>>n>>m>>s;
+    for(int i=1;i<=n;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    for(int i=i;i<=n;i++)
+    {
+        sort(g[i].begin(),g[i].end());
+    }
+}
+void bfs(int s)
+{
+    qu.push(s);
+    check[s]=1;
+    while(!qu.empty())
+    {
+        int u=qu.front();
+        qu.pop();
+        cout<<u<<" ";
+        for(auto i : g[u])
+        {
+            int v = int(i);
+            if(check[v]==0)
+            {
+                qu.push(v);
+                check[v]=1;
+            }
+        }
+    }
+}
+int main()
+{
+    inp();
+    bfs(s);
+    return 0;
+}
